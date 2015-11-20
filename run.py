43cbad72255321a5128fb52887847c4d5afa16b4
robin <robin.van.rijn@ing.nl>
2015-11-20 13:09:49 +0200

    Templates!

--- run.py
+++ run.py
@@ -3,7 +3,23 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Hello There!!</h1>"
+
+@application.route("/antigoni")
+def antigoni():
+    return "Hello Antigoni"
+
+@application.route("/robin")
+def robin():
+    return "Hello Robin"
+
+@application.route("/jens")
+def jens():
+    return "Hello Jens"
+
+@application.route("/erikos")
+def erikos():
+    return "Hello Erikos"
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', debug=True)
