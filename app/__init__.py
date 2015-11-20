bd18fd2b4a77d8c94f43f950b2523a32b04c0b0e
robin <robin.van.rijn@ing.nl>
2015-11-20 23:14:53 +0200

    123

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 #Include config from config.py
 app.config.from_object('config')
 app.secret_key = 'some_secret'
+
+
+
 #Create an instance of SQLAclhemy
 db = SQLAlchemy(app)
 
